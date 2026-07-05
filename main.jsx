cdae180a1d40db9de62010de6b9241ec81bc268e
serikovvyz-gif <serikov.vyz@gmail.com>
2026-07-05 13:44:04 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import WeddingTracker from './WeddingTracker.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <WeddingTracker />
-  </React.StrictMode>
-);
